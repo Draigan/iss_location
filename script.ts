34dd4d777f1899fcfa73dcc4dc7dd8cd6cadf79d
Draigan <draigan.lefebvre@gmail.com>
2022-10-04 21:24:38 -0400

    leafy working and getting data

---
+++ script.ts
@@ -0,0 +1,33 @@
+
+
+
+async function getISS(): Promise<void>
+{
+   let response = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
+   let json = await response.json();
+   const {latitude, longitude} = json;
+
+   const lonEl = document.getElementById('lon');
+   const latEl = document.getElementById('lat');
+
+   
+   if (lonEl){
+    lonEl.textContent = longitude;
+   }
+
+   if (latEl){
+    latEl.textContent = latitude;
+   }
+
+
+}
+
+getISS();
+
+var map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 1
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',
+ {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
